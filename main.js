2a657f366981e04518f5e75e5544a080a572b46d
Aneta Kwasek <anetakwasek96@gmail.com>
2020-02-29 09:49:21 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+alert ('siema! Nazywam się Aneta Kwasek i mam 23 lat');
+console.log ('Siema!')
+const name = 'Aneta';
+const age = 23;
+console.log (name);
+console.log (age);
+console.log(`Nazywam się ${name} i mam ${age} lat`);
+
